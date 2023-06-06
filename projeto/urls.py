bbf6009d85e4d11b31ac7e52a0ada59237cf8c9b
Victor Hugo <victor.hiraiftw@outlook.com>
2023-06-06 18:35:28 -0300

    chore: att gitignore

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.http import HttpResponse
+from django.urls import path
 
 
 def my_view(request):
