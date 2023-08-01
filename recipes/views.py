23338ef37bbebec315763cdea85bd9aef6801d02
Victor Hugo <victor.hiraiftw@outlook.com>
2023-08-01 16:21:02 -0300

    fix: corrigindo busca do search somente por receitas publicadas

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.db.models import Q
 
 from .models import Recipe
 # from django.http import HttpResponse
@@ -44,7 +45,16 @@ def search(request):
     if not search_term:
         raise Http404()
 
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/search.html', context={
         'page_title': f'Search for "{search_term}" |',
         'search_term': search_term,
+        'recipes': recipes,
     })
