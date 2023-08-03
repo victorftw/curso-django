22e5de7edfadbeacd88f05e8b5ecdc82ee9c89c1
Victor Hugo <victor.hiraiftw@outlook.com>
2023-08-03 18:32:04 -0300

    refactor: separando logica de paginação para make_pagination

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,8 @@
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.db.models import Q
-from django.core.paginator import Paginator
 
-from utils.pagination import make_pagination_range
+from utils.pagination import make_pagination
 from .models import Recipe
 # from django.http import HttpResponse
 
@@ -13,19 +12,7 @@ def home(request):
         is_published=True
     ).order_by('-id')
 
-    try:
-        current_page = int(request.GET.get('page', 1))
-    except ValueError:
-        current_page = 1
-
-    paginator = Paginator(recipes, 9)
-    page_obj = paginator.get_page(current_page)
-
-    pagination_range = make_pagination_range(
-        paginator.page_range,
-        4,
-        current_page
-    )
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
@@ -41,8 +28,11 @@ def category(request, category_id):
         ).order_by('-id')
     )
 
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
+
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
         'title': f'{recipes[0].category.name} - Category |'
     })
 
@@ -70,8 +60,12 @@ def search(request):
         is_published=True
     ).order_by('-id')
 
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
+
     return render(request, 'recipes/pages/search.html', context={
         'page_title': f'Search for "{search_term}" |',
         'search_term': search_term,
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
+        'additional_url_query': f'&q={search_term}'
     })
