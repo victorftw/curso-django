8156204de3654e60531c9b0a396415d4185fbcda
Victor Hugo <victor.hiraiftw@outlook.com>
2023-11-21 12:00:37 -0300

    feat: exibindo receitas do usuario no dashboard

--- authors/views.py
+++ authors/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from recipes.models import Recipe
 
 from .forms import LoginForm, RegisterForm
 
@@ -82,4 +83,10 @@ def logout_view(request):
 
 @login_required(login_url='authors:login', redirect_field_name='next')
 def dashboard(request):
-    return render(request, 'authors/pages/dashboard.html')
+    recipes = Recipe.objects.filter(
+        is_published=False,
+        author=request.user
+    )
+    return render(request, 'authors/pages/dashboard.html', {
+        'recipes': recipes,
+    })
