e8ab3a6c54bd84fcb672b84eace37b614fdba891
Victor Hugo <victor.hiraiftw@outlook.com>
2023-11-28 16:12:42 -0300

    feat: delete para apagar receitas

---
+++ base_static/global/js/scripts.js
@@ -0,0 +1,15 @@
+function my_scope() {
+  const forms = document.querySelectorAll('.form-delete');
+
+  for (const form of forms) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const confirmed = confirm('Are you sure?');
+      if (confirmed) {
+        form.submit();
+      }
+    });
+  }
+}
+
+my_scope();
