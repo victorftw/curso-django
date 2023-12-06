39cc17c1cf4533f0c499c95763f85e1f96184f33
Victor Hugo <victor.hiraiftw@outlook.com>
2023-12-06 17:26:20 -0300

    refactor: recipeDetail cbv

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,8 @@
 import os
 
 from django.http.response import Http404
-from django.shortcuts import get_object_or_404, render
 from django.db.models import Q
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from utils.pagination import make_pagination
 from .models import Recipe
@@ -96,10 +95,21 @@ class RecipeListViewSearch(RecipeListViewBase):
         return ctx
 
 
-def recipe(request, id):
-    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
+class RecipeDetail(DetailView):
+    model = Recipe
+    context_object_name = 'recipe'
+    template_name = 'recipes/pages/recipe-view.html'
 
-    return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': recipe,
-        'is_detail_page': True,
-    })
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(is_published=True)
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+
+        ctx.update({
+            'is_detail_page': True
+        })
+
+        return ctx
