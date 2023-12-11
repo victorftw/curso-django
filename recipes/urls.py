b6719d722a75593137fa31f7622107d05d553272
Victor Hugo <victor.hiraiftw@outlook.com>
2023-12-11 09:48:20 -0300

    feat: jsonresponse para a api que usa detailview

--- recipes/urls.py
+++ recipes/urls.py
@@ -12,4 +12,8 @@ urlpatterns = [
          views.RecipeListViewCategory.as_view(),
          name="category"),
     path('recipes/<int:pk>/', views.RecipeDetail.as_view(), name="recipe"),
+    path('recipes/api/v1/',
+         views.RecipeListViewHomeApi.as_view(), name="recipes_api_v1"),
+    path('recipes/api/v1/<int:pk>/',
+         views.RecipeDetailApi.as_view(), name="recipes_api_v1_detail"),
 ]
