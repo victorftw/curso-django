1e04b3f77cc5f70c07c037e32cbc187eb419d5e0
Victor Hugo <victor.hiraiftw@outlook.com>
2023-12-12 15:35:14 -0300

    feat: view de tags

--- recipes/urls.py
+++ recipes/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('', views.RecipeListViewHome.as_view(), name="home"),
     path('recipes/search/', views.RecipeListViewSearch.as_view(),
          name="search"),
+    path('recipes/tags/<slug:slug>/', views.RecipeListViewTag.as_view(),
+         name="tag"),
     path('recipes/category/<int:category_id>/',
          views.RecipeListViewCategory.as_view(),
          name="category"),
--- recipes/views.py
+++ recipes/views.py
@@ -10,6 +10,7 @@ from django.forms.models import model_to_dict
 
 from utils.pagination import make_pagination
 from .models import Recipe
+from tag.models import Tag
 
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
@@ -42,6 +43,7 @@ class RecipeListViewBase(ListView):
             is_published=True,
         )
         qs = qs.select_related('author', 'category')
+        qs = qs.prefetch_related('tags')
         return qs
 
     def get_context_data(self, *args, **kwargs):
@@ -95,6 +97,31 @@ class RecipeListViewCategory(RecipeListViewBase):
             'title': f'{ctx.get("recipes")[0].category.name} - Category | '
         })
 
+        return ctx
+
+
+class RecipeListViewTag(RecipeListViewBase):
+    template_name = 'recipes/pages/tag.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(tags__slug=self.kwargs.get('slug', ''))
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        page_title = Tag.objects.filter(
+                slug=self.kwargs.get('slug', '')
+            ).first()
+
+        if not page_title:
+            page_title = 'No recipes found'
+
+        page_title = f'{page_title} - Tag |'
+
+        ctx.update({
+            'page_title': page_title,
+        })
         return ctx
 
 
