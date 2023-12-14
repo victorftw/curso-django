2361af1262c34ca1d28b38953d23c2b3607fae7b
Victor Hugo <victor.hiraiftw@outlook.com>
2023-12-14 10:11:00 -0300

    chore: organizando arquivos

---
+++ project/settings/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+from .environment import * # isort:skip
+from .installed_apps import * # isort:skip
+from .middlewares import * # isort:skip
+from .templates import *
+from .databases import *
+from .security import *
+from .i18n import *
+from .assets import *
+from .messages import *
+from .debug_toolbar import * # isort:skip
---
+++ project/settings/environment.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True if os.environ.get('DEBUG') == '1' else False
+
+ALLOWED_HOSTS: list[str] = []
+
+ROOT_URLCONF = 'project.urls'
+
+WSGI_APPLICATION = 'project.wsgi.application'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
