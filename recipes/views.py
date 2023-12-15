56892f5b4ba3d798a2abcabc6ffb3397cc7c91f0
Victor Hugo <victor.hiraiftw@outlook.com>
2023-12-15 16:52:02 -0300

    feat: tradução na view

--- recipes/views.py
+++ recipes/views.py
@@ -7,6 +7,8 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
+from django.utils import translation
+from django.utils.translation import gettext as _
 
 from utils.pagination import make_pagination
 from .models import Recipe
@@ -53,8 +55,15 @@ class RecipeListViewBase(ListView):
             ctx.get('recipes'),
             PER_PAGE
         )
+
+        html_language = translation.get_language()
+
         ctx.update(
-            {'recipes': page_obj, 'pagination_range': pagination_range}
+            {
+                'recipes': page_obj,
+                'pagination_range': pagination_range,
+                'html_language': html_language,
+            }
         )
         return ctx
 
@@ -92,9 +101,11 @@ class RecipeListViewCategory(RecipeListViewBase):
 
     def get_context_data(self, *args, **kwargs):
         ctx = super().get_context_data(*args, **kwargs)
+        category_translation = _('Category')
 
         ctx.update({
-            'title': f'{ctx.get("recipes")[0].category.name} - Category | '
+            'title': f'{ctx.get("recipes")[0].category.name} - '
+            f'{category_translation} | '
         })
 
         return ctx
