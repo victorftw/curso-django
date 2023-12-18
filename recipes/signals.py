5ffc0f6ecfa0097564c0142ea8d2f01be2599162
Victor Hugo <victor.hiraiftw@outlook.com>
2023-12-18 10:16:57 -0300

    feat: apagando imagem antiga caso a imagem de recipe seja atualizada

--- recipes/signals.py
+++ recipes/signals.py
@@ -1,5 +1,5 @@
 import os
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
 from recipes.models import Recipe
 
@@ -15,3 +15,12 @@ def delete_cover(instance):
 def recipe_cover_delete(sender, instance, *args, **kwargs):
     old_instance = Recipe.objects.get(pk=instance.pk)
     delete_cover(old_instance)
+
+
+@receiver(pre_save, sender=Recipe)
+def recipe_cover_update(sender, instance, *args, **kwargs):
+    old_instance = Recipe.objects.get(pk=instance.pk)
+    is_new_conver = old_instance != instance.cover
+
+    if is_new_conver:
+        delete_cover(old_instance)
