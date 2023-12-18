aa95239190b4e3e6bfee7386127f7c0d768e256b
Victor Hugo <victor.hiraiftw@outlook.com>
2023-12-18 14:18:52 -0300

    feat: buscando campos relacionados

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,5 +1,9 @@
 from rest_framework import serializers
 
+from django.contrib.auth.models import User
+from tag.models import Tag
+from tag.serializers import TagSerializer
+
 
 class RecipeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
@@ -8,6 +12,18 @@ class RecipeSerializer(serializers.Serializer):
     public = serializers.BooleanField(source='is_published')
     preparation = serializers.SerializerMethodField(
         method_name='any_method_name',
+    )
+    category = serializers.StringRelatedField()
+    author = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+    )
+    tags = serializers.PrimaryKeyRelatedField(
+        queryset=Tag.objects.all(),
+        many=True,
+    )
+    tag_objects = TagSerializer(
+        many=True,
+        source='tags',
     )
 
     def any_method_name(self, recipe):
