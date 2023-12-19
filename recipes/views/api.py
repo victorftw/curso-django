d8ba04ba76f17292ac6f5115e1dd721e35b6abe8
Victor Hugo <victor.hiraiftw@outlook.com>
2023-12-19 16:39:43 -0300

    feat: mixins, generic view e pagination no drf

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -2,6 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.pagination import PageNumberPagination
 from django.shortcuts import get_object_or_404
 
 from ..models import Recipe
@@ -10,27 +12,34 @@ from tag.models import Tag
 from tag.serializers import TagSerializer
 
 
-class RecipeAPIv2List(APIView):
-    def get(self, request):
-        recipes = Recipe.objects.get_published()[:10]
-        serializer = RecipeSerializer(
-            instance=recipes,
-            many=True,
-            context={'request': request},
-        )
-        return Response(serializer.data)
+class RecipeAPIv2Pagination(PageNumberPagination):
+    page_size = 2
 
-    def post(self, request):
-        serializer = RecipeSerializer(
-            data=request.data,
-            context={'request': request},
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(
-            serializer.data,
-            status=status.HTTP_201_CREATED
-        )
+
+class RecipeAPIv2List(ListCreateAPIView):
+    queryset = Recipe.objects.get_published()
+    serializer_class = RecipeSerializer
+    pagination_class = RecipeAPIv2Pagination
+    # def get(self, request):
+    #     recipes = Recipe.objects.get_published()[:10]
+    #     serializer = RecipeSerializer(
+    #         instance=recipes,
+    #         many=True,
+    #         context={'request': request},
+    #     )
+    #     return Response(serializer.data)
+
+    # def post(self, request):
+    #     serializer = RecipeSerializer(
+    #         data=request.data,
+    #         context={'request': request},
+    #     )
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save()
+    #     return Response(
+    #         serializer.data,
+    #         status=status.HTTP_201_CREATED
+    #     )
 
 
 # @api_view(http_method_names=['get', 'post'])
